8916ce1693c43aa7ec8d2d1c042fe618d64b5383
Romulozz <foxes71502@gmail.com>
2023-08-24 10:46:50 -0500

    avanzando

--- src/main/java/app/repository/ProductRepository.java
+++ src/main/java/app/repository/ProductRepository.java
@@ -1,4 +1,14 @@
 package app.repository;
 
+import upeu.edu.pe.lp.infrastructure.entity.ProductEntity;
+import upeu.edu.pe.lp.infrastructure.entity.UserEntity;
+
 public interface ProductRepository {
+    //Lista de todos los productos
+    Iterable<ProductEntity> getProducts();
+    //Lista de productos por usuario
+    Iterable<ProductEntity> getProductsByUser(UserEntity user);
+    ProductEntity getProductById(Integer id);
+    ProductEntity saveProduct(ProductEntity product);
+    void deleteProductById(Integer id);
 }
---
+++ src/main/java/app/service/ProductService.java
@@ -0,0 +1,28 @@
+package app.service;
+
+import upeu.edu.pe.lp.infrastructure.entity.ProductEntity;
+import upeu.edu.pe.lp.infrastructure.entity.UserEntity;
+
+public class ProductService {
+    private final ProductService productRepository;
+
+    public ProductService(ProductService productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Iterable<ProductEntity> getProducts(){
+        return productRepository.getProducts();
+    }
+    public Iterable<ProductEntity> getProductsByUser(UserEntity user){
+        return productRepository.getProductsByUser(user);
+    }
+    public ProductEntity getProductById(Integer id){
+        return productRepository.getProductById(id);
+    }
+    public ProductEntity saveProduct(ProductEntity product){
+        return productRepository.saveProduct(product);
+    }
+    public void deleteProductById(Integer id){
+        productRepository.deleteProductById(id);
+    }
+}
