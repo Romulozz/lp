a79b9eedcf556c0b183153d08dd388b263e8ff25
Romulozz <foxes71502@gmail.com>
2023-10-12 10:13:09 -0500

    completando crud productos

--- src/main/java/upeu/edu/pe/lp/infrastructure/controller/ProductoControllerApi.java
+++ src/main/java/upeu/edu/pe/lp/infrastructure/controller/ProductoControllerApi.java
@@ -31,12 +31,12 @@ public class ProductoControllerApi {
     }
     //guardar producto
     @PostMapping("/save-product")
-    public String saveProduct(ProductEntity product,@RequestParam("img")MultipartFile multipartFile) throws IOException {
+    public String saveProduct(ProductEntity product, @RequestParam("img") MultipartFile multipartFile) throws IOException {
         log.info("Nombre de producto: {}", product);
         productService.savProduct(product, multipartFile);
-        return "admin/products/create";
-        //return "redirect:/admin";
+        return "redirect:/admin/products/show";
     }
+
 
     @GetMapping("/show")
     public String showProduct(Model model){
