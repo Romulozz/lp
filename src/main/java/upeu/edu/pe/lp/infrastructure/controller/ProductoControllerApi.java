9f0ec96290d7e62a1e4288e58ca762e9e10c8be1
Diego <alonsorengifo264@gmail.com>
2023-10-11 21:12:25 -0500

    Crando vista editar y agregar

--- src/main/java/upeu/edu/pe/lp/infrastructure/controller/ProductoControllerApi.java
+++ src/main/java/upeu/edu/pe/lp/infrastructure/controller/ProductoControllerApi.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 
 
 @Controller
-@RequestMapping("/api/v1/product/")
+@RequestMapping("/admin/products")
 public class ProductoControllerApi {
 
     private final ProductService productService;
