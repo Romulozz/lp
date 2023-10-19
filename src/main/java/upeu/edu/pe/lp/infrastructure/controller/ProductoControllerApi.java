ad710a1372f45a7d5b448bee9110b0a003c54d89
Romulozz <foxes71502@gmail.com>
2023-10-19 12:11:34 -0500

    renombrando controllers

--- src/main/java/upeu/edu/pe/lp/infrastructure/controller/ProductoControllerApi.java
+++ src/main/java/upeu/edu/pe/lp/infrastructure/controller/ProductoControllerApi.java
@@ -1,70 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package upeu.edu.pe.lp.infrastructure.controller;
 
-import org.springframework.http.HttpStatus;
+import org.slf4j.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import upeu.edu.pe.lp.app.service.ProductService;
 import upeu.edu.pe.lp.infrastructure.entity.ProductEntity;
 import upeu.edu.pe.lp.infrastructure.entity.UserEntity;
 
-/**
- *
- * @author Luki
- */
-public class ProductoControllerApi {
-    
-    @RestController
-@RequestMapping("/admin/product/")
-public class ProductControllerApi {
-    
-   private final ProductService productService;
+import java.io.IOException;
 
-    public ProductControllerApi(ProductService productService) {
+@CrossOrigin(origins = "http://localhost/4200")
+@Controller
+@RequestMapping("/api")
+public class ProductoControllerApi {
+
+    private final ProductService productService;
+    private final Logger log = LoggerFactory.getLogger(ProductoControllerApi.class);
+
+    public ProductoControllerApi(ProductService productService) {
         this.productService = productService;
     }
-    //crear product
-    @PostMapping("/save-product")
-    public String saveProduct(@RequestBody ProductEntity productEntity){
-       // return productService.saveProduct(productEntity).toString();
-       return null;
+
+    //crear nuevo producto
+    @GetMapping("/product")
+    public String create(){
+        return "admin/products/create";
     }
-     //ver productos
-    @GetMapping("/show")
-    public Iterable <ProductEntity> showProduct(){
+    //guardar producto
+    @PostMapping("/product")
+    public String saveProduct(ProductEntity product, @RequestParam("img") MultipartFile multipartFile) throws IOException {
+        log.info("Nombre de producto: {}", product);
+        productService.savProduct(product, multipartFile);
+
+        // Agregar un retraso de 1 segundo (1000 milisegundos) antes de redirigir
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/admin";
+    }
+
+
+    @GetMapping("/product")
+    public String showProduct(Model model){
+        //log.info("id user desde la variable de session: {}");
         UserEntity user = new UserEntity();
         user.setId(1);
-        return productService.getProductsByUser(user);
+        Iterable<ProductEntity> products = productService.getProductsByUser(user);
+        model.addAttribute("products", products);
+        return "admin/products/show";
     }
-    //buscar producto por Id
-    @GetMapping("/show/{id}")
-    public ProductEntity show(@PathVariable Integer id){
-       return productService.getProductById(id);
+
+
+    @GetMapping("/product/{id}")
+    public String editProduct(@PathVariable Integer id, Model model){
+        ProductEntity product = productService.getProductById(id);
+        log.info("Product obtenido: {}", product);
+        model.addAttribute("product",product);
+        return "admin/products/edit";
     }
-    //editar un product
-   @PutMapping("/edit/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ProductEntity editProduct(@RequestBody ProductEntity product, @PathVariable Integer id) {
-        ProductEntity productActual = productService.getProductById(id);
-        productActual.setDescription(product.getDescription());
-        productActual.setName(product.getName());
-        productActual.setPrice(product.getPrice());
-        productActual.setUserEntity(product.getUserEntity());
-       // return productService.saveProduct(productActual);
-       return null;
-       // log.info("Product obtenido: {}", product);
-        //model.addAttribute("product", product);
-        //return "admin/products/edit";
-    }
-    //eliminar un product
-     @DeleteMapping("/delete/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteProduct(@PathVariable Integer id) {
+
+    @GetMapping("/product/{id}")
+    public String deleteProduct(@PathVariable Integer id){
         productService.deleteProductById(id);
-       // return "redirect:/admin/products/show";
+        return "redirect:/admin";
     }
-}
-    
 }
