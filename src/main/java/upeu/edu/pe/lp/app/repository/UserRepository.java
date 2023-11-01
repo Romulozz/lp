f5184a69fe4623a1b697b48f4f0c9e4bc5b5896b
0xM4v1k <luiseduardogamer974@gmail.com>
2023-10-31 22:31:58 -0500

    Se agregó los métodos a UserRepository

--- src/main/java/upeu/edu/pe/lp/app/repository/UserRepository.java
+++ src/main/java/upeu/edu/pe/lp/app/repository/UserRepository.java
@@ -4,10 +4,17 @@
  */
 package upeu.edu.pe.lp.app.repository;
 
+import upeu.edu.pe.lp.infrastructure.entity.UserEntity;
+
 /**
  *
  * @author Luki
  */
 public interface UserRepository {
     
+    UserEntity getUserById(Integer id);
+    UserEntity findByEmail(String email);
+    UserEntity saveUser(UserEntity user);
+    //UserEntity updateUser(UserEntity user);
+    void deleteUserById(Integer id);
 }
