61a057206bf1f8589c1485f7efe56709a05a8547
0xM4v1k <luiseduardogamer974@gmail.com>
2023-10-31 22:46:04 -0500

    Se agregó la lógica de UserService

--- src/main/java/upeu/edu/pe/lp/app/service/UserService.java
+++ src/main/java/upeu/edu/pe/lp/app/service/UserService.java
@@ -4,10 +4,68 @@
  */
 package upeu.edu.pe.lp.app.service;
 
+import java.util.Objects;
+import upeu.edu.pe.lp.app.repository.UserRepository;
+import upeu.edu.pe.lp.infrastructure.entity.UserEntity;
+
 /**
  *
  * @author Luki
  */
 public class UserService {
+   
+private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     
+    public UserEntity findByEmail(String email) {
+        
+        UserEntity existingUser = userRepository.findByEmail(email);
+        
+            if(existingUser.getEmail().equals(email)){
+                return null;
+            }else{
+                return userRepository.findByEmail(email);
+            }
+    }
+    
+    public UserEntity saveUser(UserEntity user) {
+        UserEntity existingUser = userRepository.findByEmail(user.getEmail());
+        
+         if (user.getId() == null){
+          /*
+             existingUser.setId(1);
+             existingUser.setAddress(address);
+             existingUser.setCellphone(cellphone);
+             existingUser.setDataCreated(LocalDateTime.now());
+             existingUser.setEmail(email);
+             existingUser.setFirtsname(firtsname);
+             existingUser.setLastname(lastname);
+             existingUser.setPassword(password);
+             existingUser.setUsername(username);
+         */
+             return userRepository.saveUser(existingUser);
+         }else{
+             UserEntity productDB = userRepository.getUserById(user.getId());
+             
+             return productDB;
+         }
+    }
+
+    public UserEntity updateUser(UserEntity user) {
+        UserEntity userExists = new UserEntity();
+        if (!Objects.equals(user.getId(), userExists)) {
+           userExists.setUsername(user.getUsername());
+            userExists.setEmail(user.getEmail());
+            // Actualiza otros campos según tus necesidades
+            return userExists;
+        }
+        return null; // Devolver null si no se encuentra el usuario
+    }
+
+    public void deleteUserById(Integer id) {
+       userRepository.deleteUserById(id);
+    }    
 }
