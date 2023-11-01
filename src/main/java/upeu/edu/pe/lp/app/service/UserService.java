a228f15267c0b2a1506013844070ad4bf8e94477
0xM4v1k <luiseduardogamer974@gmail.com>
2023-10-31 22:45:19 -0500

    Se agregó UserService

---
+++ src/main/java/upeu/edu/pe/lp/app/service/UserService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upeu.edu.pe.lp.app.service;
+
+/**
+ *
+ * @author Luki
+ */
+public class UserService {
+    
+}
