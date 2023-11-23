a0b61f2fd15a3494678dd80c5f9b5818a7b76da3
Romulozz <foxes71502@gmail.com>
2023-11-23 12:24:03 -0500

    Agregando form de register admin del admin

--- src/main/java/upeu/edu/pe/lp/infrastructure/controller/RegisterAdminController.java
+++ src/main/java/upeu/edu/pe/lp/infrastructure/controller/RegisterAdminController.java
@@ -1,48 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package upeu.edu.pe.lp.infrastructure.controller;
 
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import upeu.edu.pe.lp.app.service.RegistrationService;
-import upeu.edu.pe.lp.app.service.UserService;
+import upeu.edu.pe.lp.infrastructure.dto.AdminDto;
 import upeu.edu.pe.lp.infrastructure.dto.UserDto;
-import upeu.edu.pe.lp.infrastructure.entity.UserEntity;
 
-/**
- *
- * @author Luki
- */
 @Controller
-@RequestMapping("/register")
-public class RegisterController {
-
+@RequestMapping("/register-admin")
+public class RegisterAdminController {
     private final RegistrationService registrationService;
     private final Logger log = LoggerFactory.getLogger(RegisterController.class);
 
-    public RegisterController(RegistrationService registrationService) {
+    public RegisterAdminController(RegistrationService registrationService) {
         this.registrationService = registrationService;
     }
 
     @GetMapping
-    public String showRegisterForm(UserDto userDto) {
+    public String showRegisterForm(AdminDto adminDto) {
 
-        return "login/register"; //
+        return "admin/register"; //
     }
 
     @PostMapping
-    public String registerUser(@Valid UserDto userDto, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+    public String registerUser(@Valid AdminDto adminDto, BindingResult bindingResult, RedirectAttributes redirectAttributes){
 //        user.setDateCreated(LocalDateTime.now());
 //        user.setUserType(UserType.USER);
 //        user.setUsername(user.getEmail());
@@ -53,7 +41,7 @@ public class RegisterController {
             );
             return "/login/register";
         }
-        registrationService.register(userDto.userDtoToUser());
+        registrationService.register(adminDto.userDtoToUser());
         redirectAttributes.addFlashAttribute("success", "Usuario creado correctamente");
 
 
@@ -66,7 +54,5 @@ public class RegisterController {
 
         return "redirect:/login";
     }
-
-
 
 }
