a879ee3e69483f6015414a031be22a1a9feafc4e
DiegoAlonso26 <alonsorengifo264@gmail.com>
2025-03-30 15:37:16 -0500

    cambios

--- src/main/java/upeu/edu/pe/lp/LpApplication.java
+++ src/main/java/upeu/edu/pe/lp/LpApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LpApplication {
+public class
+LpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LpApplication.class, args);
