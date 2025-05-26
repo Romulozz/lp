6585fdcbc27145059d520e087cb6eca319de0abe
DiegoAlonso26 <alonsorengifo264@gmail.com>
2025-05-25 21:14:55 -0500

    Nuevo .yml

--- src/main/java/upeu/edu/pe/lp/LpApplication.java
+++ src/main/java/upeu/edu/pe/lp/LpApplication.java
@@ -3,7 +3,8 @@ package upeu.edu.pe.lp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootTest
+@ActiveProfiles("test")
 public class LpApplication {
 
 	public static void main(String[] args) {
