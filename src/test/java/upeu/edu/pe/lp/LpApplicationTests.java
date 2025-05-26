ba1d76c8330b734eb81f6ad0a651f92d027d0ffa
DiegoAlonso26 <alonsorengifo264@gmail.com>
2025-05-25 21:34:07 -0500

    Agregando MockBean

--- src/test/java/upeu/edu/pe/lp/LpApplicationTests.java
+++ src/test/java/upeu/edu/pe/lp/LpApplicationTests.java
@@ -3,11 +3,16 @@ package upeu.edu.pe.lp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class LpApplicationTests {
+
+	@MockBean
+	private JavaMailSender javaMailSender;
+
 
 	@Test
 	void contextLoads() {
