fbc8c531b3ff8bf890fa2a69af42bd95b146fb2e
DiegoAlonso26 <alonsorengifo264@gmail.com>
2025-05-25 21:20:29 -0500

    Agregando ActiveProfiles

--- src/main/java/upeu/edu/pe/lp/LpApplication.java
+++ src/main/java/upeu/edu/pe/lp/LpApplication.java
@@ -3,8 +3,7 @@ package upeu.edu.pe.lp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootApplication
 public class LpApplication {
 
 	public static void main(String[] args) {
