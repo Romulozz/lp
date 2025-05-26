80d8478aa55aa59cc094af7048b976b032e2fd8b
DiegoAlonso26 <alonsorengifo264@gmail.com>
2025-05-25 21:25:01 -0500

    importar ActiveProfiles

--- src/test/java/upeu/edu/pe/lp/LpApplicationTests.java
+++ src/test/java/upeu/edu/pe/lp/LpApplicationTests.java
@@ -2,6 +2,8 @@ package upeu.edu.pe.lp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
